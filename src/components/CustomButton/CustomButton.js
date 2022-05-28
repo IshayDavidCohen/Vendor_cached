a86cd994dbb9206df8310bd232c264aa462ed224
Ishay Cohen <IshayDavidCohen@gmail.com>
2022-05-28 23:46:29 +0300

    Adding: New Button type "Secondary"
    Signed-off-by: Ishay Cohen <IshayDavidCohen@gmail.com>

--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -28,14 +28,22 @@ const styles = StyleSheet.create({
     container_PRIMARY: {
         backgroundColor: '#3B71F3',
     },
+    container_SECONDARY: {
+        borderColor: '#3B71F3',
+        borderWidth: 2,
+
+    },
     container_TERTIARY: {},
       
     text: {
         fontWeight: 'bold',
         color: 'white'
     },
+    text_SECONDARY: {
+        color: '#3B71F3'
+    },
     text_TERITARY: {
-        color: 'gray'
+        color: 'gray',
     },
 })
 export default CustomButton
