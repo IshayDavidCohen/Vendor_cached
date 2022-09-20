7bbacaf6a447b463245a6b743587063634739b93
Ishay Cohen <IshayDavidCohen@gmail.com>
2022-09-20 15:49:13 +0300

    Change: Marketplace.js now utilizes Populate
    Signed-off-by: Ishay Cohen <IshayDavidCohen@gmail.com>

--- src/screens/Marketplace/Marketplace.js
+++ src/screens/Marketplace/Marketplace.js
@@ -1,25 +1,31 @@
-import { View, Text, SafeAreaView} from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, ScrollView} from 'react-native'
+import React, {useState, useEffect} from 'react'
 import CustomButton from '../../components/CustomButton/CustomButton'
-import SlideWidget from '../../components/SlideWidget/SlideWidget'
 import { useNavigation } from '@react-navigation/native'
+import PopulateMarket from './Populate'
 
 
 const Marketplace = () => {
 
-const navigation = useNavigation();
-
-const onSignIn = () => {
-  navigation.navigate('SignIn');
-}
-
-  return (
-    <SafeAreaView>
-      <Text style={{fontSize: 24, alignSelf: 'center', textAlign: 'center'}}>Home Screen{"\n"} Under Development.</Text>
-      <CustomButton text='Back to Sign in [Temporary]' onPress={onSignIn} type='TERITARY'/>
-      <SlideWidget/>
-    </SafeAreaView>
-  )
+  const navigation = useNavigation();
+  const [marketData, setMarketData] = useState([]);
+  useEffect(async () => {
+    setMarketData(await PopulateMarket());
+    console.log("Marketplace rendered")
+  }, [setMarketData])
+  const onSignIn = () => {
+    navigation.navigate('SignIn');
+  }
+  
+    return (
+      <SafeAreaView>
+        <Text style={{fontSize: 24, alignSelf: 'center', textAlign: 'center'}}>Vendor{"\n"}</Text>
+        <ScrollView>
+          {marketData}
+        </ScrollView>
+        <CustomButton text='Back to Sign in [Temporary]' onPress={onSignIn} type='TERITARY'/>
+      </SafeAreaView>
+    )
 }
 
 export default Marketplace
