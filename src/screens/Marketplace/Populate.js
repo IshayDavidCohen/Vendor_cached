b81208c4eb95a7aa4c650e39646035e4393a73b8
Ishay Cohen <IshayDavidCohen@gmail.com>
2022-09-20 15:48:25 +0300

    Marketplace: added PopulateMarket & Profile
    *Dynamically gets the amount of SlideWidget components based on category or items
    
    Signed-off-by: Ishay Cohen <IshayDavidCohen@gmail.com>

---
+++ src/screens/Marketplace/Populate.js
@@ -0,0 +1,26 @@
+import { Text } from 'react-native'
+import React, {useState, useEffect, useCallback, useRef} from 'react'
+import { getMarketplaceData, readData} from '../../utils/firebase/Database'
+import SlideWidget from '../../components/SlideWidget/SlideWidget'
+
+const navigatePopulation = (loc) => {
+  return {'navigateTo': loc};
+}
+
+const PopulateMarket = async () => {
+  const data = await getMarketplaceData();
+  let slideList = [];
+  Object.entries(data).map(([k, v]) => {
+      slideList.push(
+          <Text key={`${k}_0`} style={{fontSize: 18, fontWeight: 'bold', color: '#051C60', margin: 10}}>{k}</Text>,
+          <SlideWidget key={`${k}_1`} data={v} onPress={() => navigatePopulation('Profile')}/>)
+  })
+  return await slideList
+}
+
+export const PopulateProfile = async (profileName) => {  
+  const data = await readData(profileName);
+  return data['Items'];
+}
+
+export default PopulateMarket
