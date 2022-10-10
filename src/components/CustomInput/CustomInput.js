774b980554d63de27b1745fa4e14b927b783eba0
Ishay Cohen <IshayDavidCohen@gmail.com>
2022-10-10 13:26:47 +0300

    CustomInput: Digit-Only input added
    * CustomInput now has the ability to have digit-only input types with a set of rules to prevent bugs etc.

--- src/components/CustomInput/CustomInput.js
+++ src/components/CustomInput/CustomInput.js
@@ -1,17 +1,37 @@
 import react from "react";
 import { View, Text, TextInput, StyleSheet} from 'react-native'
 
-const CustomInput = ({value, setValue, placeholder, secureTextEntry}) => {
+const CustomInput = ({value, setValue, placeholder, type='container', secureTextEntry, font}) => {
+    const handleInput = () => {
+        if (type.includes('digit')) {
+            // Digit input logic
+            
+            // Remove non-digit characters
+            value = value.replace(/[^0-9]/g, '')
+            
+            // Remove leading zeros
+            if (value.startsWith('0') && value.length > 1) {
+                value = parseInt(value, 10).toString();
+            }
+
+            // Empty input = 0
+            if (value.length === 0) {
+                value = '0';
+            }
+        }
+    }
+    handleInput()
+
     return (
-        <View style={styles.container}>
+        <View style={styles[type]}>
             <TextInput 
             value={value}
             onChangeText={setValue}
             placeholder={placeholder}
-            style={styles.input}
+            style={[styles.input, font ? {fontSize: font} : {}]}
             secureTextEntry={secureTextEntry}/>
         </View>
-    );
+    )
 }
 
 const styles= StyleSheet.create({
@@ -26,6 +46,32 @@ const styles= StyleSheet.create({
         paddingVertical: 10,
         paddingHorizontal: 10,
         marginVertical: 5,
+    },
+    digit: {
+        width: '35%',
+        borderRadius: 15,
+        alignItems: 'center',
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        marginVertical: 5,
+    },
+    digitFilled: {
+        width: '40%',
+        backgroundColor: 'white',
+        alignItems: 'center',
+        borderRadius: 15,
+
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        marginVertical: 5,
+
+        shadowColor: "#000000",
+        shadowOpacity: 0.05,
+        shadowRadius: 1,
+        shadowOffset: {
+          height: 1,
+          width: 0
+        },
     },
     input: {},
 })
