86d2b24fa57d89eabe26068bda6bd76a92d2a77d
Ishay Cohen <IshayDavidCohen@gmail.com>
2022-11-22 14:50:40 +0200

    Added GlobalParams (custom useContext hook)
    Signed-off-by: Ishay Cohen <IshayDavidCohen@gmail.com>

---
+++ src/components/GlobalParams/GlobalParams.js
@@ -0,0 +1,23 @@
+import React, {useState, useMemo, useContext} from 'react'
+
+export const userContext = React.createContext();
+
+export const getState = () => {
+    return useContext(userContext); 
+}
+
+const GlobalParams = ({children}) => {
+
+    const [userParams, setUserParams] = useState({
+        'cart': {}
+    });
+
+    const valueProvider = useMemo(() => ({userParams, setUserParams}), [userParams, setUserParams]);
+  return (
+    <userContext.Provider value={valueProvider}>
+        {children}
+    </userContext.Provider>
+  )
+}
+
+export default GlobalParams
