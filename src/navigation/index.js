0897ba20123902100aa1b04e7685037f7898fd27
Ishay Cohen <IshayDavidCohen@gmail.com>
2024-04-24 12:17:16 +0300

    Navigation: Updated with new screens

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -14,7 +14,17 @@ import NewPasswordScreen from '../screens/NewPasswordScreen';
 import Marketplace from '../screens/Marketplace';
 import Profile from '../screens/ProfileScreen/Profile';
 import ViewScreen from '../screens/ViewScreen/ViewScreen';
+import CartScreen from '../screens/CartScreen/CartScreen';
+import CheckoutScreen from '../screens/CheckoutScreen/CheckoutScreen';
+import SettingsScreen from '../screens/SettingsScreen/SettingsScreen';
+import SearchScreen from '../screens/SearchScreen/SearchScreen';
+import DevScreen from '../screens/DevScreen/DevScreen';
+import EditItemScreen from '../screens/EditItemScreen/EditItemScreen';
+import UpdateScreen from '../screens/UpdateScreen/UpdateScreen';
+
+// Utilities
 import GlobalParams from '../components/GlobalParams/GlobalParams';
+import HistoryScreen from '../screens/HistoryScreen/HistoryScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -22,8 +32,8 @@ const Tab = createBottomTabNavigator();
 // To be removed in Phase 4
 const Signed = false;
 
+
 const TabNav = () => {
-  console.log('Test')
   return (
     <Tab.Navigator screenOptions={{headerShown: false}}>
       <Tab.Screen name="Home" component={Marketplace} options={{
@@ -32,26 +42,26 @@ const TabNav = () => {
         <MaterialCommunityIcons name="home" color={color} size={size} />
       ),
     }}/>
-      <Tab.Screen name="Browse" component={Marketplace} options={{
-      tabBarLabel: 'Browse',
+      <Tab.Screen name="Updates" component={UpdateScreen} options={{
+      tabBarLabel: 'Updates',
       tabBarIcon: ({ color, size }) => (
-        <MaterialCommunityIcons name="basket" color={color} size={size} />
+        <MaterialCommunityIcons name="bell" color={color} size={size} />
       ),
     }}/>
-      <Tab.Screen name="Search" component={NewPasswordScreen} options={{
+      <Tab.Screen name="Search" component={SearchScreen} options={{
       tabBarLabel: 'Search',
       tabBarIcon: ({ color, size }) => (
         <MaterialCommunityIcons name="compass" color={color} size={size} />
       ),
     }}/>
-      <Tab.Screen name="Cart" component={ResetPasswordScreen} options={{
+      <Tab.Screen name="Cart" component={CartScreen} options={{
       tabBarLabel: 'Cart',
       tabBarIcon: ({ color, size }) => (
-        <MaterialCommunityIcons name="cart" color={color} size={size} />
+        <MaterialCommunityIcons name="shopping" color={color} size={size} />
       ),
     }}/>
-      <Tab.Screen name="Settings" component={ResetPasswordScreen} options={{
-      tabBarLabel: 'Profile',
+      <Tab.Screen name="Settings" component={SettingsScreen} options={{
+      tabBarLabel: 'Settings',
       tabBarIcon: ({ color, size }) => (
         <MaterialCommunityIcons name="cog" color={color} size={size} />
       ),
@@ -63,17 +73,22 @@ const TabNav = () => {
 const Navigation = () => {
   return (
     <GlobalParams>
-    <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName={Signed ? "Marketplace" : "SignIn"}>
-      <Stack.Screen name="SignIn" component={SignInScreen} />
-      <Stack.Screen name="SignUp" component={SignUpScreen} />
-      <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} />
-      <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
-      <Stack.Screen name="NewPassword" component={NewPasswordScreen} />
-      <Stack.Screen name="Marketplace" component={TabNav} />
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName={Signed ? "Marketplace" : "SignIn"}>
+        <Stack.Screen name="SignIn" component={SignInScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
+        <Stack.Screen name="ConfirmEmail" component={ConfirmEmailScreen} />
+        <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
+        <Stack.Screen name="DevScreen" component={DevScreen} />
+        <Stack.Screen name="EditItemScreen" component={EditItemScreen} />
+        <Stack.Screen name="NewPassword" component={NewPasswordScreen} />
+        <Stack.Screen name="Marketplace" component={TabNav} />
 
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="View" component={ViewScreen} />
-    </Stack.Navigator>
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="View" component={ViewScreen} />
+        <Stack.Screen name="Checkout" component={CheckoutScreen} />
+        <Stack.Screen name="HistoryScreen" component={HistoryScreen} />
+
+      </Stack.Navigator>
     </GlobalParams>
 
   )
